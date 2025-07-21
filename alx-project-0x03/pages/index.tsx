e15e43552c4fd4cfe615b8c7e28d24ddfda058f9
BBBenson <bbbenson26@gmail.com>
2025-07-21 15:33:18 +0000

    edited - index page

--- alx-project-0x03/pages/index.tsx
+++ alx-project-0x03/pages/index.tsx
@@ -1,14 +1,13 @@
-import Button from "@/components/common/Button"
-import { useRouter } from "next/router"
-import { type PageRouteProps } from "@/interface"
+import Button from "@/components/common/Button";
+import { useRouter } from "next/router";
+import type { PageRouteProps } from "@/interface";
 
 export default function Home() {
-  const router = useRouter()
+  const router = useRouter(); // <- here
 
-  // Imperative routing with useRouter
   const routeToNextPage = ({ pageRoute }: PageRouteProps) => {
-    router.push(pageRoute, undefined, { shallow: false })
-  }
+    router.push(pageRoute, undefined, { shallow: false });
+  };
 
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col justify-center items-center text-center">
@@ -40,5 +39,5 @@ export default function Home() {
         />
       </div>
     </div>
-  )
+  );
 }
